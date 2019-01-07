95bf885a45ab2b360af1fc235efa9ad4b599bf33
Apple Coffee <734719526@qq.com>
2019-01-07 16:25:11 +0800

    添加列表

--- Assets/Scrpit/Component/UI/UIGameSkillCpt.cs
+++ Assets/Scrpit/Component/UI/UIGameSkillCpt.cs
@@ -7,11 +7,39 @@ public class UIGameSkillCpt : BaseUIComponent
 {
     //返回按钮
     public Button btBack;
+    public Text tvBack;
 
+    //标题
+    public Text tvTitle;
+    //分数
+    public Text tvScore;
+
+
+    public GameDataCpt gameDataCpt;
     private void Start()
     {
         if (btBack != null)
             btBack.onClick.AddListener(BTBack);
+        if (tvBack != null)
+            tvBack.text = GameCommonInfo.GetTextById(36);
+        if (tvTitle != null)
+            tvTitle.text = GameCommonInfo.GetTextById(33);
+    }
+
+    /// <summary>
+    /// 数据更新
+    /// </summary>
+    private void Update()
+    {
+        if (gameDataCpt == null)
+            return;
+        if (tvScore != null)
+        {
+            string outNumberStr;
+            UnitUtil.UnitEnum outUnit;
+            UnitUtil.DoubleToStrUnit(gameDataCpt.userData.userScore, out outNumberStr, out outUnit);
+            tvScore.text = outNumberStr + " " + GameCommonInfo.GetUnitStr(outUnit);
+        }
     }
 
     /// <summary>

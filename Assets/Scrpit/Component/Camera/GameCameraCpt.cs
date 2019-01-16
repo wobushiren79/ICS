5d84edbd90d552d075f307df41487c4093732e2c
Apple Coffee <734719526@qq.com>
2019-01-16 18:48:41 +0800

    添加新图标

--- Assets/Scrpit/Component/Camera/GameCameraCpt.cs
+++ Assets/Scrpit/Component/Camera/GameCameraCpt.cs
@@ -52,7 +52,7 @@ public class GameCameraCpt : BaseMonoBehaviour {
         //获取场景位置
         Vector3 levelScenesPosition= gameScenes.GetScenesPositionByLevel(level);
         //设置摄像头位置
-        gameCamera.transform.position = new Vector3(x, levelScenesPosition.y+6.5f,-7);
+        gameCamera.transform.position = new Vector3(x-2, levelScenesPosition.y+6.5f,-7);
         gameCamera.transform.rotation = new Quaternion();
         gameCamera.transform.Rotate(new Vector3(50,0,0));
         //gameCamera.transform.LookAt(levelScenesPosition);

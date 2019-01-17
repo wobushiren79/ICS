c872b9e7efbd7116bb1400bfd649b0e5d87a3f40
Apple Coffee <734719526@qq.com>
2019-01-17 20:29:44 +0800

    增加图标

---
+++ Assets/Scrpit/Component/Item/GameBufferItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+public class GameBufferItem : PopupReplyView
+{
+
+    public Button btItem;
+
+    private void Start()
+    {
+        if (btItem != null)
+            btItem.onClick.AddListener(Test);
+    }
+
+    public void Test()
+    {
+        LogUtil.Log("Test");
+    }
+    public override void ClosePopup()
+    {
+       
+    }
+
+    public override void OpenPopup()
+    {
+        infoPopupView.SetInfoData(null,"test",null,null,null,null);
+    }
+}

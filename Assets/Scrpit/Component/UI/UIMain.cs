c68af77dec1a9eb9cd3ae6cf04b4d0a059fa45d2
Apple Coffee <734719526@qq.com>
2019-02-22 17:06:16 +0800

    增加制作者名单

---
+++ Assets/Scrpit/Component/UI/UIMain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class UIMain : BaseUIManager
+{
+    public Button btMaker;
+
+    private void Start()
+    {
+        if (btMaker != null)
+            btMaker.onClick.AddListener(OpenMaker);
+    }
+
+    public void OpenMaker()
+    {
+        OpenUIAndCloseOtherByName("Maker");
+    }
+
+}
---
+++ Assets/Scrpit/Component/UI/UIMainMakerCpt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class UIMainMakerCpt : BaseUIComponent
+{
+    public Button btBack;
+    public Text tvBack;
+
+    private void Start()
+    {
+        if (btBack != null)
+            btBack.onClick.AddListener(OnClickBack);
+        if (tvBack != null)
+            tvBack.text = GameCommonInfo.GetTextById(4);
+    }
+
+    public void OnClickBack()
+    {
+        uiManager.OpenUIAndCloseOtherByName("Start");
+    }
+}

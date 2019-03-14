939d7f15bb585c042b1c01bb1bdeb92dfddfef38
Apple Coffee <734719526@qq.com>
2019-03-14 20:42:17 +0800

    转盘制作

---
+++ Assets/Scrpit/Component/Game/GameTurntableCpt.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEditor;
+using DG.Tweening;
+
+public class GameTurntableCpt : BaseMonoBehaviour
+{
+    public GameObject turntableTableObj;
+    public CallBack callBack;
+    public ParticleSystem saucePS;
+
+    public double betSorce;
+    public void StartGame(double bet, CallBack callBack)
+    {
+        this.betSorce = bet;
+        ResetTurntable();
+        this.callBack = callBack;
+
+        float randomPro = Random.Range(0f, 1f);
+        float axisRotate = 0;
+        int rewardType = 0;
+        if (randomPro <= 0.7f)
+        {
+            axisRotate = Random.Range(2f, 178f);
+            rewardType = 0;
+        }
+        else if (randomPro > 0.7f && randomPro <= 0.9f)
+        {
+            axisRotate = Random.Range(182f, 268f);
+            rewardType = 1;
+        }
+        else if (randomPro > 0.9f && randomPro <= 0.99f)
+        {
+            axisRotate = Random.Range(272f, 313f);
+            rewardType = 2;
+        }
+        else
+        {
+            axisRotate = Random.Range(317f, 358f);
+            rewardType = 3;
+        }
+
+        if (turntableTableObj == null)
+            return;
+        turntableTableObj.transform.DOLocalAxisRotate(new Vector3(0, 0, 14400 + axisRotate), 10).SetEase(Ease.InOutQuart).OnComplete(delegate ()
+        {
+            RewardDeal(rewardType);
+        });
+    }
+
+    /// <summary>
+    /// 得奖处理
+    /// </summary>
+    /// <param name="rewardType"></param>
+    public void RewardDeal(int rewardType)
+    {
+        float delay = 0;
+        bool isPlayPS = false;
+        switch (rewardType)
+        {
+            case 0:
+                betSorce = 0;
+                break;
+            case 1:
+                betSorce = betSorce * 2f;
+                delay = 5f;
+                isPlayPS = true;
+                break;
+            case 2:
+                betSorce = betSorce * 3f;
+                delay = 5f;
+                isPlayPS = true;
+                break;
+            case 3:
+                betSorce = betSorce * 5f;
+                delay = 5f;
+                isPlayPS = true;
+                break;
+        }
+        if (isPlayPS)
+        {
+            saucePS.Play();
+        }
+        transform.DOScale(new Vector3(1, 1, 1), delay).OnComplete(delegate() {
+            if (callBack != null)
+                callBack.EndGame(betSorce);
+        });
+    }
+
+    public void ResetTurntable()
+    {
+        turntableTableObj.transform.DOKill();
+        turntableTableObj.transform.localRotation = new Quaternion();
+    }
+
+    public interface CallBack
+    {
+        void EndGame(double reward);
+    }
+}
--- Assets/Scrpit/Component/UI/UIGameTurntableCpt.cs
+++ Assets/Scrpit/Component/UI/UIGameTurntableCpt.cs
@@ -2,7 +2,7 @@
 using UnityEditor;
 using UnityEngine.UI;
 using DG.Tweening;
-public class UIGameTurntableCpt : BaseUIComponent
+public class UIGameTurntableCpt : BaseUIComponent,GameTurntableCpt.CallBack
 {
     //返回按钮
     public Button btBack;
@@ -27,6 +27,7 @@ public class UIGameTurntableCpt : BaseUIComponent
 
     public GameDataCpt gameDataCpt;
     public GameToastCpt gameToastCpt;
+    public GameTurntableCpt gameTurntableCpt;
 
     private bool mIsStart = false;
     private double mBetScore = 0;
@@ -62,16 +63,18 @@ public class UIGameTurntableCpt : BaseUIComponent
             gameToastCpt.ToastHint(GameCommonInfo.GetTextById(109));
             return;
         }
-        //mIsStart = true;
+        mIsStart = true;
         StartPre();
     }
 
     //开始准备
     public void StartPre()
     {
+        gameTurntableCpt.ResetTurntable();
         //移除分数
         mBetScore = gameDataCpt.userData.userScore;
         gameDataCpt.userData.userScore = 0;
+        gameDataCpt.SaveUserData();
         //移除分数动画
         GameObject itemRemoveScoreObj=  Instantiate(tvChangeScoreOne, tvChangeScoreOne.transform);
         itemRemoveScoreObj.transform.SetParent(tfChangeOneFather);
@@ -82,7 +85,7 @@ public class UIGameTurntableCpt : BaseUIComponent
         CanvasGroup itemRemoveScoreCG= itemRemoveScoreObj.GetComponent<CanvasGroup>();
         itemRemoveScoreCG.alpha = 1;
         itemRemoveScoreCG.DOFade(0, 3);
-        itemRemoveScoreObj.transform.DOScale(new Vector3(3, 3, 3), 3).OnComplete(delegate ()
+        itemRemoveScoreObj.transform.DOScale(new Vector3(5, 5, 5), 3).OnComplete(delegate ()
         {
             Destroy(itemRemoveScoreObj);
         });
@@ -91,7 +94,7 @@ public class UIGameTurntableCpt : BaseUIComponent
         itemAddScoreObj.transform.SetParent(tfChangeTwoFather);
         itemAddScoreObj.SetActive(true);
         itemAddScoreObj.transform.position = tvChangeScoreTwo.transform.position;
-        itemAddScoreObj.transform.localScale=new Vector3(3,3,3);
+        itemAddScoreObj.transform.localScale=new Vector3(5,5,5);
         Text itemAddScoreTV = itemAddScoreObj.GetComponent<Text>();
         itemAddScoreTV.text = GameCommonInfo.GetPriceStr(mBetScore);
         itemAddScoreTV.color = new Color(0,1,0);
@@ -102,6 +105,7 @@ public class UIGameTurntableCpt : BaseUIComponent
         {
             tvBet.text = GameCommonInfo.GetPriceStr(mBetScore);
             Destroy(itemAddScoreObj);
+            gameTurntableCpt.StartGame(mBetScore,this);
         });
     }
 
@@ -118,5 +122,54 @@ public class UIGameTurntableCpt : BaseUIComponent
             return;
         }
         uiManager.OpenUIAndCloseOtherByName("GameMenu");
+    }
+
+    public void EndGame(double reward)
+    {
+        tvBet.text = GameCommonInfo.GetPriceStr(0);
+        if (reward == 0)
+        {
+            //移除分数动画
+            GameObject itemRemoveScoreObj = Instantiate(tvChangeScoreTwo, tvChangeScoreTwo.transform);
+            itemRemoveScoreObj.transform.SetParent(tfChangeTwoFather);
+            itemRemoveScoreObj.SetActive(true);
+            itemRemoveScoreObj.transform.position = tvChangeScoreTwo.transform.position;
+            Text itemRemoveScoreTV = itemRemoveScoreObj.GetComponent<Text>();
+            itemRemoveScoreTV.text = GameCommonInfo.GetPriceStr(mBetScore);
+            itemRemoveScoreTV.color = new Color(1, 0, 0);
+            CanvasGroup itemRemoveScoreCG = itemRemoveScoreObj.GetComponent<CanvasGroup>();
+            itemRemoveScoreCG.alpha = 1;
+            itemRemoveScoreCG.DOFade(0, 3);
+            itemRemoveScoreObj.transform.DOScale(new Vector3(5, 5, 5), 3).OnComplete(delegate ()
+            {
+                tvBet.text = GameCommonInfo.GetPriceStr(0);
+                Destroy(itemRemoveScoreObj);
+            });
+            mBetScore = 0;
+            mIsStart = false;
+            return;
+        }
+        //移除动画
+        mBetScore = reward;
+        //添加分数动画
+        GameObject itemAddScoreObj = Instantiate(tvChangeScoreOne, tvChangeScoreOne.transform);
+        itemAddScoreObj.transform.SetParent(tfChangeOneFather);
+        itemAddScoreObj.SetActive(true);
+        itemAddScoreObj.transform.position = tvChangeScoreOne.transform.position;
+        itemAddScoreObj.transform.localScale = new Vector3(5, 5, 5);
+        Text itemAddScoreTV = itemAddScoreObj.GetComponent<Text>();
+        itemAddScoreTV.text = GameCommonInfo.GetPriceStr(mBetScore);
+        itemAddScoreTV.color = new Color(0, 1, 0);
+        CanvasGroup itemAddScoreCG = itemAddScoreObj.GetComponent<CanvasGroup>();
+        itemAddScoreCG.alpha = 0;
+        itemAddScoreCG.DOFade(1, 3);
+        itemAddScoreObj.transform.DOScale(new Vector3(1, 1, 1), 3).OnComplete(delegate ()
+        {
+            gameDataCpt.userData.userScore += mBetScore;
+            mBetScore = 0;
+            Destroy(itemAddScoreObj);
+            mIsStart = false;
+        });
+
     }
 }
